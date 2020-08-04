8db8cf4933fac00e68fc0b1a9f38a3827c274f39
takeshi <panda531531@gmail.com>
2020-08-04 14:46:26 +0900

    add image resize preprocess

--- TitanGenerator.py
+++ TitanGenerator.py
@@ -2,6 +2,8 @@ import sys
 import os
 import subprocess
 import glob
+import cv2
+import numpy as np
 
 
 class TitanGenerator(object):
@@ -9,10 +11,14 @@ class TitanGenerator(object):
         self.input_image = args.input_image
         input_basename = os.path.splitext(
             os.path.basename(self.input_image))[0]
+        self.temp_image = os.path.join(
+            './temp/', os.path.basename(self.input_image))
         self.pifu_obj = os.path.join('./PIFu/results/pifu_demo/',
                                      'result_' + input_basename + '.obj')
         self.obj2chem_input = os.path.join('./PIFu/results/pifu_demo/',
                                            input_basename + '.obj')
+
+        self.resize = args.resize
         # remove_bg
         self.kernel_size = args.kernel_size
         self.iteration = args.iteration
@@ -34,16 +40,67 @@ class TitanGenerator(object):
             sys.path.append(remove_bg_path)
 
     def generate(self):
+        print('***************************** preprocess *****************************')
+        self._preprocess_image()
         print('***************************** remove_bg *****************************')
         self._remove_background()
         print('******************************* PIFu *******************************')
         self._apply_PIFu()
         print('*************************** obj2schematic ***************************')
         self._convert_obj2schematic()
+        print('***************************** postprocess *****************************')
+        self._postprocess()
+
+    def _preprocess_image(self):
+        print('start image preprocess....')
+        target_formats = ['.png', '.jpg', '.jpeg']
+        if not os.path.splitext(self.input_image)[1].lower() in target_formats:
+            raise Exception('input_image format should be ' +
+                            str(target_formats))
+        # read image
+        org_img = cv2.imread(self.input_image)
+        # calc resize factor
+        h, w, c = org_img.shape
+        if w > h:
+            factor = self.resize / w
+            new_size = int(h * factor)
+            new_shape = (self.resize, new_size)
+        else:
+            factor = self.resize / h
+            new_size = int(w * factor)
+            new_shape = (new_size, self.resize)
+        new_img = cv2.resize(org_img, new_shape,
+                             interpolation=cv2.INTER_LANCZOS4)
+        # add margin
+        half_margin = int((self.resize - new_size) / 2)
+        if w > h:
+            margin_val = np.full((half_margin, self.resize, 3), 255)
+            new_img = np.concatenate([margin_val, new_img, margin_val], axis=0)
+        else:
+            margin_val = np.full((self.resize, half_margin, 3), 255)
+            new_img = np.concatenate([margin_val, new_img, margin_val], axis=1)
+        # save preprocessed image
+        os.makedirs('./temp/', exist_ok=True)
+        cv2.imwrite(self.temp_image, new_img)
+        print('finish image preprocess....')
+
+    def _postprocess(self):
+        print('cleaning....')
+        # remove_bg
+        for p in glob.glob('./remove_bg/output/*.png'):
+            if os.path.isfile(p):
+                os.remove(p)
+
+        # temp image
+        for p in glob.glob('./temp/*'):
+            if os.path.isfile(p):
+                os.remove(p)
+
+        os.rmdir('./temp/')
 
     def _remove_background(self):
         commands = ['python', 'remove_bg/BackgroundRemover.py',
-                    self.input_image, '--kernel_size', str(self.kernel_size), '--iteration', str(self.iteration)]
+                    self.temp_image, '--kernel_size', str(self.kernel_size), '--iteration', str(self.iteration)]
         subprocess.run(commands)
 
     def _apply_PIFu(self):
@@ -78,11 +135,6 @@ class TitanGenerator(object):
         commands.extend(mlp_colors)
         subprocess.run(commands)
 
-        # postprocess
-        for p in glob.glob('./remove_bg/output/*.png'):
-            if os.path.isfile(p):
-                os.remove(p)
-
     def _convert_obj2schematic(self):
         # PIFuの出力ファイル名に'result_'が入っているのでファイル名を変更
         os.rename(self.pifu_obj, self.obj2chem_input)
@@ -97,6 +149,8 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("input_image",
                         help="input .png image path")
+    parser.add_argument('--resize', type=int, default=512,
+                        help='preprocessed image size')
     parser.add_argument("--kernel_size", type=int, default=5)
     parser.add_argument("--iteration", type=int, default=3)
     parser.add_argument('--output_dir', type=str, default=None,
